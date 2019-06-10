71807b5d65e59ed10f2776a1cd104fe92786dac4
Robert Feussner <robert.feussner@tum.de>
2019-06-10 20:02:23 +0200

    minor change for file tests

--- Test.py
+++ Test.py
@@ -4,3 +4,7 @@ import torchvision
 t = torch.randn(5,3)
 print(t)
 print("Cuda available: ", torch.cuda.is_available())
+f = open('Test.txt', 'w+')
+for i in range(10):
+    f.write("This is line %d\r\n" % (i+1))
+f.close()
