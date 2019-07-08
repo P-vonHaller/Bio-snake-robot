e015f4f9c920cbf23459e37dcab4a3cbf0cf943f
Robert Feussner <robert.feussner@tum.de>
2019-07-08 10:35:16 +0200

    Change of colours

--- Pytorch-Deeplab/PlotLossAndAccuracy.py
+++ Pytorch-Deeplab/PlotLossAndAccuracy.py
@@ -13,16 +13,16 @@ for line in l.readlines():
 	if line[0] == 's':
 		break
 	if line[0] != 't':
-		xl.append(int(line.split(';')[0]))
-		yl.append(int(line.split(';')[1]))
+		xl.append(float(line.split(',')[0]))
+		yl.append(float(line.split(',')[1]))
 l.close()
 
 a = open(ACC, 'r')
 xa = [0]
 ya = [0]
 for line in a.readlines():
-	xa.append(int(line.split(';')[0]))
-	ya.append(int(line.split(';')[1]))
+	xa.append(float(line.split(';')[0]))
+	ya.append(float(line.split(';')[1]))
 a.close()
 
 
@@ -31,7 +31,7 @@ fig, ax = plt.subplots()
 ax.plot(xl, yl, 'r')
 ax.plot(xa, ya, 'b')
 
-ax.set(xlabel='Number Steps', ylabel='Accuracy(red)/Loss(blue)',title='Loss and Accuracy')
+ax.set(xlabel='Number Steps', ylabel='Accuracy(blue)/Loss(red)',title='Loss and Accuracy')
 
 #plt.show()
 fig.savefig('some.png')
