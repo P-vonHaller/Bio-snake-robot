dc290aafc08208f415f9b739d438670af46a2974
PeiskerT <peisker-tim@web.de>
2019-07-21 16:57:51 +0200

    test load

--- layer15.py
+++ layer15.py
@@ -42,7 +42,7 @@ def main():
     for i in range(0, 10574):
 
         b11 = torch.load(b11_location + "/predictions" + str(i) + ".pth")
-        print(type(b11))
+        print(torch.size(b11))
         if counter == 1: break
         counter += 1
 
